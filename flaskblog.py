5f96f6153e3e62feb495f0c9ed0d76c9e12bf938
KelvinHalx <56820692+KelvinHalx@users.noreply.github.com>
2020-06-01 15:25:38 +0300

    Update flaskblog.py

--- flaskblog.py
+++ flaskblog.py
@@ -28,21 +28,26 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route("/register", methods=['GET','POST'])
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for{form.username.data}!','sucess')
+        flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route("/login", methods=['GET','POST'])
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        flash('You have been logged in!','sucess')
-        return redirect(url_for('home'))
-    return render_template('register.html', title='Register', form=form)
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
+
 
 
 if __name__ == '__main__':
